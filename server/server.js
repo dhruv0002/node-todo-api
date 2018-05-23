17720674f517bea88c35fbfc2cc78e82a65a0edf
dhruv0002 <dhruv0002@gmail.com>
2018-05-23 08:23:20 +0530

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -60,6 +61,27 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;//It is used to grab id from the url 'localhost:3000/todos/5b04ca909b0dad40d0a1d8db'
+    
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo)
+        {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+    }, (e) => {
+        res.status(404).send();
+    });
+    
 });
 
 app.listen(3000, () => {
